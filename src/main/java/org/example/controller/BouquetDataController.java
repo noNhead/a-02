c07313e8720b2f21c01d02e51ec5570dd03e05d1
noNhead <sun495fun@gmail.com>
2021-05-11 13:55:38 +0400

    aaaa

--- src/main/java/org/example/controller/BouquetDataController.java
+++ src/main/java/org/example/controller/BouquetDataController.java
@@ -1,39 +1,23 @@
 package org.example.controller;
 
 import org.example.data.gen.Bouquet;
-import org.example.data.gen.GetBouquetRequest;
-import org.example.data.gen.GetBouquetResponse;
-import com.fasterxml.jackson.databind.util.ClassUtil;
+import org.example.service.BouquetService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 
 @RestController
 public class BouquetDataController {
     private static final Logger LOGGER = LoggerFactory.getLogger(BouquetDataController.class);
-    private static final String address = "http://localhost:8080/ws/GetBouquetResponse";
-
-    private final Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
 
     @GetMapping("bouquet/{name}")
     @PostConstruct
     public String getBouquetFromDatabase(@PathVariable String name) throws Exception {
-        if (marshaller.getPackagesToScan() == null) {
-            marshaller.setPackagesToScan(ClassUtil.getPackageName(GetBouquetRequest.class));
-            marshaller.afterPropertiesSet();
-        }
-
-        WebServiceTemplate webServiceTemplate = new WebServiceTemplate(marshaller);
-        GetBouquetRequest request = new GetBouquetRequest();
-        request.setName(name);
-
-        GetBouquetResponse response = (GetBouquetResponse) webServiceTemplate.marshalSendAndReceive(address, request);
-
-        Bouquet bouquet = response.getBouquet();
+        Bouquet bouquet = BouquetService.getResponseBouquet(name);
         LOGGER.info("Received bouquet: " + bouquet.getName() + ", with description: " + bouquet.getDescription()+";");
         return "Received bouquet: " + bouquet.getName() + ", with description: " + bouquet.getDescription()+";";
     }
--- src/main/java/org/example/service/BouquetService.java
+++ src/main/java/org/example/service/BouquetService.java
@@ -1,13 +1,21 @@
 package org.example.service;
 
+import com.fasterxml.jackson.databind.util.ClassUtil;
 import org.example.data.gen.Bouquet;
+import org.example.data.gen.GetBouquetRequest;
+import org.example.data.gen.GetBouquetResponse;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Service;
+import org.springframework.ws.client.core.WebServiceTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class BouquetService {
+    private static final String address = "http://localhost:8080/ws/GetBouquetResponse";
+    private final Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+
     public List<Bouquet> getAllBouquets() {
         Bouquet bouquet1 = new Bouquet();
         bouquet1.setName("name-1");
@@ -26,5 +34,20 @@ public class BouquetService {
         bouquets.add(bouquet2);
         bouquets.add(bouquet3);
         return bouquets;
+    }
+
+    public Bouquet getResponseBouquet(String name) throws Exception {
+        if (marshaller.getPackagesToScan() == null) {
+            marshaller.setPackagesToScan(ClassUtil.getPackageName(GetBouquetRequest.class));
+            marshaller.afterPropertiesSet();
+        }
+
+        WebServiceTemplate webServiceTemplate = new WebServiceTemplate(marshaller);
+        GetBouquetRequest request = new GetBouquetRequest();
+        request.setName(name);
+
+        GetBouquetResponse response = (GetBouquetResponse) webServiceTemplate.marshalSendAndReceive(address, request);
+
+        return response.getBouquet();
     }
 }
